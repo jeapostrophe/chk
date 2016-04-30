ffac7b2c4be9b70b9ebaaa6d904f8eda8f904708
Jay McCarthy <jay.mccarthy@gmail.com>
2016-04-30 19:42:21 -0400

    Documentation

--- chk.scrbl
+++ chk.scrbl
@@ -1,10 +1,241 @@
 #lang scribble/manual
-@require[@for-label[chk
-                    racket/base]]
+@(require (for-label chk
+                     racket/base)
+          scribble/example
+          racket/sandbox)
+
+@(define e (make-base-eval '(require chk)))
 
 @title{chk: a minimal tester}
-@author+email["Jay McCarthy" "jay@racket-lang.org"]
+@author{@author+email["Jay McCarthy" "jay@racket-lang.org"]}
 
-@defmodule[rackunit/chk]
+@defmodule[chk]
 
-XXX
+This package defines a minimal testing library with a simple
+implementation, sensible defaults, useful flexibilty, and beautiful
+failure reporting. It is independent of all other Racket testing
+libraries, but it does cooperate with @exec{raco test} and @exec{raco
+cover} for displaying summary information.
+
+@defform[(chk . t)]{
+
+Evaluates the tests described in @racket[t] as described below:
+
+@racketgrammar*[[t (test ...)]
+                [test
+                 strict-test
+                 (strict-test)
+                 (ex ex)
+                 (ex)]
+                [strict-test
+                 (#:t ex)
+                 (#:? ex ex)
+                 (#:= ex ex)
+                 (#:eq ex ex ex)
+                 (#:! test)
+                 (#:x ex ex)
+                 (#:do e)]
+                [ex
+                 (#:stx e e)
+                 (#:src e e)
+                 e]]}
+
+The syntax of @racket[_t] determines the test executed.
+
+@section{Single Value Tests}
+
+The simplest form is a @racket[#:t _e] test, where @racketmodname[chk]
+ensures that @racket[_e] evaluates to exactly one value that is not
+@racket[#f].
+
+@examples[#:eval e
+          (chk #:t 5)
+          (chk #:t #f)
+          (chk #:t (values 1 2))
+          (chk #:t (/ 1 0))]
+
+This may be abbreviated by removing the @racket[#:t].
+
+@examples[#:eval e
+          (chk 5)
+          (chk #f)]
+
+A @racket[#:t _e] test is a special case of a @racket[#:? _p _e] test,
+where @racketmodname[chk] ensures that @racket[_e] evaluates to
+exactly one value and that the predicate @racket[_p] does not return
+@racket[#f] on it. This form is robust to errors in @racket[_p], which
+must evaluate to exactly one value that is a procedure of one
+argument.
+
+@examples[#:eval e
+          (chk #:? even? 0)
+          (chk #:? even? 1)
+          (chk #:? even? (values 1 2))
+          (chk #:? even? "two")
+          (chk #:? #f 0)
+          (chk #:? (values 3 4) 0)
+          (chk #:? list-ref 0)
+          (chk #:? (error 'boo) 0)]
+
+@section{Expected Value Tests}
+
+An @racket[#:= _actual _expected] test compares an actual evaluation
+with an expected evaluation. If @racket[_expected] raises an
+exception, then @racket[_actual] must also, with the same
+@racket[exn-message]. Otherwise, the two expression must return the
+same number of values, which must be @racket[equal?].
+
+@examples[#:eval e
+          (chk #:= 1 1)
+          (chk #:= 1 "two")
+
+          (chk #:= (/ 1 0) (error '/ "division by zero"))
+          (chk #:= (/ 1 0) +inf.0)
+          (chk #:= (/ 1 0) (error '/ "divided by zero"))
+          
+          (chk #:= (values 1 2) (values 1 2))
+          (chk #:= (values 1 2) 1)
+          (chk #:= 1 (values 1 2))
+          (chk #:= (values 1 2) (values "one" "two"))]
+
+An @racket[#:= _actual _expected] test may be abbreviated as
+@racket[_actual _expected].
+
+@examples[#:eval e
+          (chk 1 1)
+          (chk 1 "two")]
+
+An @racket[#:= _actual _expected] test is a special case of a
+@racket[#:eq _equal _actual _expected] test where @racket[_equal] is
+@racket[equal?]. @racket[_equal] must evaluate to exactly one value,
+which is a procedure of two arguments. If @racket[_equal] does not
+evaluate as expected, then the test is considered a failure.
+
+@examples[#:eval e
+          (chk #:eq = 1 1)
+          (chk #:eq eq? (cons 1 2) (cons 1 2))
+          (chk #:eq (/ 1 0) 1 2)
+          (chk #:eq #f 1 1)
+          (chk #:eq add1 1 1)
+          (chk #:eq + "one" "two")]
+
+@section{Expected Exception Tests}
+
+An @racket[#:x _actual _exn-pred] test is form when you expect
+@racket[_actual] to evaluate to an exception, but cannot (or do not
+want to) predict the exact error message. @racket[_exn-pred] must
+evaluate to exactly one value. If that value is a string, then it must
+be appear in the message of the exception that @racket[_actual]
+raises. If that value is a regular expression, then it must match the
+message of exception that @racket[_actual] raises. If the value is not
+a string or regular expression, it must be a procedure of one argument
+that does not return @racket[#f] on the exception that
+@racket[_actual] raises.
+
+@examples[#:eval e
+          (chk #:x (/ 1 0) "division")
+          (chk #:x (/ 1 0) "divided")
+          
+          (chk #:x (/ 1 0) #rx"di.ision")
+          (chk #:x (/ 1 0) #rx"(one|two)")
+
+          (chk #:x (/ 1 0) exn:fail?)
+          (chk #:x (/ 1 0) exn:fail:syntax?)
+
+          (chk #:x 0 exn:fail?)
+          (chk #:x (/ 1 0) 0)
+          (chk #:x (/ 1 0) cons)
+          (chk #:x (/ 1 0) add1)]
+
+@section{Meta Forms}
+
+There are two meta-forms provided by @racket[chk].
+
+A @racket[#:! _t] test, where @racket[_t] is another test, represents
+the expectation that test @racket[_t] will fail.
+
+@examples[#:eval e
+          (chk #:! #:t #f)
+          (chk #:! #:t #t)
+          
+          (chk #:! #:= 1 0)
+          (chk #:! #:= 1 1)
+          
+          (chk #:! #:x (/ 1 0) exn:fail:syntax?)
+          (chk #:! #:x (/ 1 0) "division")]
+
+A @racket[#:do _e] form is equivalent to @racket[_e]. (This does not
+appear to be useful, unless you read the next section!)
+
+@examples[#:eval e
+          (chk #:do (printf "Hey, listen!"))]
+
+@section{Combining Tests}
+
+A @racket[chk] form can contain any number of sub-forms. This is why
+@racket[#:do] can be useful. They may be wrapped in parentheses, or
+not.
+
+@examples[#:eval e
+          (chk 1 1
+               2 2
+               #t)
+          (chk #:= 1 1
+               #:! #:t #f
+               #:? even? 2)
+          (chk [#:= 1 1]
+               [#:! #:t #f]
+               [#:? even? 2])
+          (chk #:! [#:t #f]
+               #:! [#:= 1 2])
+          (chk #:= (+ 2 3) 5
+               #:do (define x 1)
+               #:= (+ 5 x) 6)]
+
+@section{Test Sets}
+
+@defform[(chk* body ...+)]{
+
+Sometimes you want to ensure that if one test fails, then no other
+tests are evaluated within a block. @racket[chk*] does this for all
+tests within @racket[(let () body ...)].
+
+@examples[#:eval e
+          (chk*
+           (define x "one")
+           (chk #:? number? x
+                #:do (printf "Hey, listen!\n")
+                #:? even? x)
+           (chk #:? odd? (add1 x)))]}
+
+@section{Contextual Failure Reporting}
+
+@defform[(with-chk ([key value] ...) body ...+)]{
+
+The failure reporting format used by @racket[chk] is sensitive to the
+dynamic context it is run within. The @racket[with-chk] form allows
+you to add details to all tests within @racket[(let () body ...)].
+
+@examples[#:eval e
+          (with-chk (['author "jay"])
+            (chk #:= 1 1
+                 #:= 1 2)
+            (with-chk (['suite "1337"])
+              (chk #:? even? "two")))]
+
+}
+
+@defthing[chk-inform! symbol?]{
+
+A special key for use with @racket[with-chk] that is not printed, but
+instead must be matched with a value that is a procedure of one
+argument. If there is an error, then the function will be called with
+all of the failure details.
+
+@examples[#:eval e
+          (with-chk (['author "jay"]
+                     [chk-inform! (λ (d) (printf "Woah:\n~v\n" d))])
+            (chk #:= 1 1
+                 #:= 1 2))]
+
+}
--- main.rkt
+++ main.rkt
@@ -1,32 +1,21 @@
 #lang racket/base
 (require racket/match
          racket/list
+         racket/function
          syntax/parse/define
          (for-syntax racket/base
                      syntax/parse
                      syntax/srcloc)
          syntax/quote
          syntax/srcloc
-         rackunit/log)
-
-(define-syntax-rule (D e stx)
-  (let ([stx-id stx])
-    (with-handlers ([exn? (λ (x) (res:exn stx-id x))])
-      (call-with-values (λ () e)
-                        (λ vs (res:values stx-id vs))))))
-
-(define with-chk-param (make-parameter null))
-
-(define-simple-macro (with-chk ([k v] ...) e ...+)
-  (parameterize ([with-chk-param
-                   (cons (list (cons k v) ...)
-                         (with-chk-param))])
-    (let () e ...)))
-
-(require racket/pretty
+         rackunit/log
+         racket/pretty
          racket/format
          racket/string
          racket/port)
+
+;; Display code
+(define chk-inform! (gensym 'chk-inform!))
 (define (format-v v)
   (match v
     [(? syntax?)
@@ -51,29 +40,42 @@
                 [_ (cons 'values x)]))
              (format-v stx))]
     [_
-     (pretty-format v)]))
+     (pretty-format #:mode 'write v)]))
 (define (display-info d)
   (eprintf "FAILURE\n")
   (unless (null? d)
     (define key-symbols (map car d))
     (define key-strings (map symbol->string key-symbols))
     (define max-length (apply max (map string-length key-strings)))
-    (for ([d (in-list d)])
-      (match-define (cons k v) d)
-      (define k-header
-        (format "~a : "
-                (~a #:min-width max-length
-                    #:align 'left
-                    k)))
-      (define k-space-header
-        (make-string (string-length k-header) #\space))
-      (eprintf "~a" k-header)
-      (for ([vl (in-list (string-split (format-v v) "\n"))]
-            [i (in-naturals)])
-        (eprintf "~a~a\n"
-                 (if (zero? i) "" k-space-header)
-                 vl))))
+    (for ([the-d (in-list d)])
+      (match-define (cons k v) the-d)
+      (cond
+        [(eq? k chk-inform!)
+         (v d)]
+        [else
+         (define k-header
+           (format "~a : "
+                   (~a #:min-width max-length
+                       #:align 'left
+                       k)))
+         (define k-space-header
+           (make-string (string-length k-header) #\space))
+         (eprintf "~a" k-header)
+         (for ([vl (in-list (string-split (format-v v) "\n"))]
+               [i (in-naturals)])
+           (eprintf "~a~a\n"
+                    (if (zero? i) "" k-space-header)
+                    vl))])))
   (eprintf "\n"))
+
+;; Recording check details
+(define with-chk-param (make-parameter null))
+
+(define-simple-macro (with-chk ([k v] ...) e ...+)
+  (parameterize ([with-chk-param
+                   (cons (list (cons k v) ...)
+                         (with-chk-param))])
+    (let () e ...)))
 
 (define (*chk-escape! v)
   (unless v
@@ -83,6 +85,7 @@
        (with-chk-param)))))
   (test-log! v))
 
+;; Context manipulation
 (define current-chk-escape (make-parameter *chk-escape!))
 (define (*chk-fail!) ((current-chk-escape) #f))
 (define (*chk-succ!) ((current-chk-escape) #t))
@@ -109,10 +112,21 @@
 (define-syntax-rule (*chk-invert e)
   (*chk-invert* (λ () e)))
 
-(struct res (stx))
-(struct res:exn res (x))
-(struct res:values res (vs))
+;; Evaluating expressions
+(struct res (stx) #:prefab)
+(struct res:exn res (x) #:prefab)
+(struct res:values res (vs) #:prefab)
 
+(define-syntax-rule (D e stx)
+  (let ([stx-id stx])
+    (with-handlers ([exn? (λ (x) (res:exn stx-id x))])
+      (call-with-values (λ () e)
+                        (λ vs (res:values stx-id vs))))))
+
+(define D-identity (D identity #'identity))
+(define D-equal? (D equal? #'equal?))
+
+;; Core checks
 (define (*check= p = a e)
   (with-chk (['part p]
              ['actual a]
@@ -125,49 +139,94 @@
     (unless v
       (*chk-fail!))))
 
-(define (check-equal? a e)
-  (with-chk (['kind "equal?"]
-             ['actual a]
-             ['expected e])
-    (match e
-      [(? res:exn?)
-       (chk*
-        (*check? "exception?" (res:exn? a))
-        (*check= "exn-message"
-                 string=?
-                 (exn-message (res:exn-x a))
-                 (exn-message (res:exn-x e)))
-        (*chk-succ!))]
-      [(? res:values?)
-       (chk*
-        (*check? "values" (res:values? a))
-        (define as (res:values-vs a))
-        (define es (res:values-vs e))
-        (*check= "how many values"
-                 =
-                 (length as)
-                 (length es))
-        (for ([a (in-list as)]
-              [e (in-list es)]
-              [i (in-naturals)])
-          (*check= (format "value ~a" i)
-                   equal?
-                   a
-                   e))
-        (*chk-succ!))])))
+(define (protect p f)
+  (λ args
+    (with-handlers ([exn:fail?
+                     (λ (x)
+                       (with-chk (['part (format "evaluating ~a" p)]
+                                  ['exn x])
+                         (*chk-fail!)))])
+      (apply f args))))
 
-(define (check-not-false a)
-  (with-chk (['kind "not false"]
-             ['actual a])
-    (chk*
-     (*check? "values" (res:values? a))
-     (define as (res:values-vs a))
-     (cond
-       [(= 1 (length as))
-        (*check? "not false" (first as))
-        (*chk-succ!)]
-       [else
-        (*chk-succ!)]))))
+(define (*check-one-value v)
+  (*check? "not exception" (res:values? v))
+  (define v-vs (res:values-vs v))
+  (*check= "how many values" = (length v-vs) 1)
+  (car v-vs))
+
+(define (*check-one-value-matching-? p ? v)
+  (*check? p (? (*check-one-value v))))
+
+(define (valid-compare? x)
+  (and (procedure? x)
+       (procedure-arity-includes? x 2)))
+
+(define (check eq a e)
+  (chk*
+   (with-chk (['kind "valid compare expected"]
+              ['actual eq])
+     (*check-one-value-matching-?
+      "procedure of 2 args" valid-compare? eq))
+   (match-define (res:values _ (list the-eq)) eq)
+   (with-chk (['kind "compare"]
+              ['compare the-eq]
+              ['actual a]
+              ['expected e])
+     (match e
+       [(? res:exn?)
+        (chk*
+         (*check? "exception?" (res:exn? a))
+         (*check= "exn-message"
+                  string=?
+                  (exn-message (res:exn-x a))
+                  (exn-message (res:exn-x e)))
+         (*chk-succ!))]
+       [(? res:values?)
+        (chk*
+         (*check? "values" (res:values? a))
+         (define as (res:values-vs a))
+         (define es (res:values-vs e))
+         (*check= "how many values"
+                  =
+                  (length as)
+                  (length es))
+         (for ([a (in-list as)]
+               [e (in-list es)]
+               [i (in-naturals)])
+           (*check=
+            (format "value ~a" i)
+            (protect "check" the-eq)
+            a
+            e))
+         (*chk-succ!))]))))
+
+(define (valid-pred? x)
+  (and (procedure? x)
+       (procedure-arity-includes? x 1)))
+
+(define (check? ? a)
+  (chk*
+   (with-chk (['kind "valid predicate expected"]
+              ['actual ?])
+     (*check-one-value-matching-?
+      "procedure of 1 arg" valid-pred? ?))
+   (match-define (res:values _ (list the-?)) ?)
+   (with-chk (['kind "predicate"]
+              ['predicate the-?]
+              ['actual a])
+     (chk*
+      (define v (*check-one-value a))
+      (with-chk (['value v])
+        (*check? "not false" ((protect "predicate" the-?) v))
+        (*chk-succ!))))))
+
+(define (*chk-regexp-match s r)
+  (and (regexp-match r s) #t))
+
+(define (valid-exn-expect? x)
+  (or (string? x) (regexp? x)
+      (and (procedure? x)
+           (procedure-arity-includes? x 1))))
 
 (define (check-exn a e)
   (chk*
@@ -177,17 +236,12 @@
        [(? res:exn?)
         (void)]
        [(? res:values?)
-        (define es (res:values-vs e))
-        (*check= "how many values" = (length es) 1)
-        (define x (first es))
-        (*check? "string, regexp, or procedure of 1 arg"
-                 (or (string? x) (regexp? x)
-                     (and (procedure? x)
-                          (procedure-arity-includes? x 1))))]))
+        (*check-one-value-matching-?
+         "exception expectation" valid-exn-expect? e)]))
    (with-chk (['kind "exception"])
      (match e
        [(? res:exn?)
-        (check-equal? a e)]
+        (check D-equal? a e)]
        [(res:values _ (list e))
         (with-chk (['actual a])
           (*check? "exception" (res:exn? a))
@@ -202,13 +256,12 @@
                       (exn-message x)
                       e)]
             [(? procedure?)
-             (*check? (object-name e) (e x))]))]))))
+             (*check? (object-name e) ((protect "exn predicate" e) x))])
+          (*chk-succ!))]))))
 
-(define (*chk-regexp-match s r)
-  (and (regexp-match r s) #t))
-
+;; Syntax
 (begin-for-syntax
-  (define-syntax-class test-expr
+  (define-syntax-class test-expr-inner
     #:attributes (stx e)
     [pattern (#:stx s e)
              #:attr stx #'(quote-syntax/keep-srcloc s)]
@@ -218,6 +271,11 @@
     [pattern e:expr
              #:attr stx #'(quote-syntax/keep-srcloc e)])
 
+  (define-syntax-class test-expr
+    #:attributes (exp)
+    [pattern t:test-expr-inner
+             #:attr exp #'(D t.e t.stx)])
+
   (define-splicing-syntax-class strict-test
     #:commit
     #:attributes (unit)
@@ -226,15 +284,23 @@
     [pattern (~seq #:t a:test-expr)
              #:attr unit
              (syntax/loc #'a
-               (check-not-false (D a.e a.stx)))]
+               (check? D-identity a.exp))]
+    [pattern (~seq #:? ?:test-expr a:test-expr)
+             #:attr unit
+             (syntax/loc #'a
+               (check? ?.exp a.exp))]
     [pattern (~seq #:x a:test-expr b:test-expr)
              #:attr unit
              (syntax/loc #'a
-               (check-exn (D a.e a.stx) (D b.e b.stx)))]
+               (check-exn a.exp b.exp))]
+    [pattern (~seq #:eq eq?:test-expr a:test-expr b:test-expr)
+             #:attr unit
+             (syntax/loc #'a
+               (check eq?.exp a.exp b.exp))]
     [pattern (~seq #:= a:test-expr b:test-expr)
              #:attr unit
              (syntax/loc #'a
-               (check-equal? (D a.e a.stx) (D b.e b.stx)))]
+               (check D-equal? a.exp b.exp))]
     [pattern (~seq #:do e:expr)
              #:attr unit #'e])
 
@@ -259,7 +325,11 @@
 
 (provide chk
          chk*
-         with-chk)
+         with-chk
+         chk-inform!)
+
+(module+ test
+  (chk #:! #:x (/ 1 0) "division"))
 
 (module+ test
   (chk
@@ -285,14 +355,15 @@
    #:! (error 'fail "a") (error 'fail "b")
 
    #:! #:t (/ 1 0)
-   #:t (values 0 1)
-   #:t (values #f 1)
    ;; Fail 5
+   #:t (values 0 1)
+   ;; Fail 6
+   #:t (values #f 1)
    #:! #:t (values #f 1)
 
    1 1
    2 2
-   ;; Fail 6
+   ;; Fail 7
    #:x (/ 1 0) "divided"
    #:x (/ 1 0) "division"
    #:x (/ 1 0) #rx"di.ision"
@@ -309,16 +380,19 @@
    #:t 1
    #:! 2 3
 
+   ;; It just works with values
    (values 1 2) (values 1 2)
-   ;; Fail 7
+   ;; Fail 8
    (values 1 2) (values 2 3)
    #:! (values 1 2) (values 2 3)
-   ;; Fail 8
+   ;; Fail 9
    (values 1 2) 3
    #:! (values 1 2) 3
    #:! 3 (values 1 2)
    (quotient/remainder 10 3) (values 3 1)
 
+   ;; You can make things more explicit, if you want, by adding parens
+   ;; and #:=
    #:= 1 1
    [#:x (/ 1 0) "division"]
    [#:! #f]
@@ -326,13 +400,13 @@
    [#:= 1 1]
 
    ;; You can change what the printed source AND location is
-   ;; Fail 9
+   ;; Fail 10
    #:= [#:stx one 1] 2
 
    ;; You can also change what JUST source location is
-   ;; Fail 10
-   #:= [#:src #'here 1] 2
    ;; Fail 11
+   #:= [#:src #'here 1] 2
+   ;; Fail 12
    #:= [#:src '(there 99 1 22 1) 1] 2
 
    ;; You can put in arbitrary code and definitions:
@@ -346,11 +420,11 @@
    ;; immediately put a big chk form)
    #:do
    (chk* (chk #:= 1 1
-              ;; Fail 12
+              ;; Fail 13
               #:= 2 3
               ;; NOT printed
               #:do (printf "Hey, listen!")
-              ;; NOT Fail 13
+              ;; NOT Fail 14
               #:= 5 6))
 
    ;; with-chk allows you to add information to all failures contained
@@ -358,8 +432,27 @@
    #:do
    (with-chk (['module "testing"]
               ['author "jay"])
-     ;; Fail 13
+     ;; Fail 14
      (chk 1 2)
      (with-chk (['amazing? #t])
-       ;; Fail 14
-       (chk 2 3)))))
+       ;; Fail 15
+       (chk 2 3)))
+
+   ;; #:eq allows you to specify a comparison
+   #:eq eq? 'foo 'foo
+   #:! #:eq eq? 'foo 'bar
+   ;; Fail 16
+   #:eq eq? "foo" (list->string (string->list "foo"))
+   ;; Fail 17
+   #:eq string=? "foo" 'bar
+   ;; Fail 18
+   #:eq (/ 1 0) 0 0
+
+   ;; #:? allows you to specify a predicate
+   #:? even? 0
+   ;; Fail 19
+   #:? even? 1
+   ;; Fail 20
+   #:? even? "two"
+   ;; Fail 21
+   #:? (/ 1 0) 0))
