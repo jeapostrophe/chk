0c1a20bc95725ee082ae73dff7da016a14950ea0
shhyou <shu-hung.you@eecs.northwestern.edu>
2021-04-15 16:31:13 -0500

    Do not remove dependency on base at runtime

--- chk-test/info.rkt
+++ chk-test/info.rkt
@@ -1,6 +1,5 @@
 #lang info
 (define collection 'multi)
-(define deps '("chk-lib" ("testing-util-lib" #:version "1.1")))
-(define build-deps '("base"))
+(define deps '("base" "chk-lib" ("testing-util-lib" #:version "1.1")))
 (define update-implies '("chk-lib"))
 (define pkg-desc "Tests for \"chk\".")
