1989e32684d67dcc9763faf4ec833f7d3d9e04cb
shhyou <shu-hung.you@eecs.northwestern.edu>
2021-04-15 17:32:31 -0500

    The usual package is not dependent on the tests

--- chk/info.rkt
+++ chk/info.rkt
@@ -1,4 +1,4 @@
 #lang info
 (define collection 'multi)
-(define deps '("chk-lib" "chk-doc" "chk-test"))
-(define implies '("chk-lib" "chk-doc" "chk-test"))
+(define deps '("chk-lib" "chk-doc"))
+(define implies '("chk-lib" "chk-doc"))
